eb17116606f954349812eeda455a760d2b0277a4
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-03-15 15:26:35 -0300

    Update fetchAjax.js

--- fetchAjax.js
+++ fetchAjax.js
@@ -1,6 +1,16 @@
+/*
+* JavaScript fetch with JQuery Ajax similiar syntax
+* @param {string} path - file path or url to fetch
+* @param {string} method - PHP method: GET(default) or POST
+* @param {FormData} formData - FormData for fetch's body(optional)
+* @param {HTMLElement} form - form with datas for fetch's body(optional)
+* @param {object} data - datas for fetch's body(optional)
+* @param {string} dataType - response's data type: auto(default), json, blob or text
+*/
 async function fetchAjax({
-	url: url,
+	path: path,
 	method: method,
+	formData: formData,
 	form: form,
 	data: data,
 	dataType: dataType
@@ -8,10 +18,10 @@ async function fetchAjax({
 
 	try{
 
-		const res = await fetch(url, {
+		const res = await fetch(path, {
 
-			method: method,
-			body: createFormData({
+			method: method || 'GET',
+			body: formData || createFormData({
 				form: form,
 				data: data
 			})
@@ -21,7 +31,7 @@ async function fetchAjax({
 			throw new Error(`HTTP ERROR: ${res.status}`)
 		}
 		const decrypted_res =
-			!dataType
+			!dataType || dataType == 'auto'
 				? false
 				:(
 					dataType == 'json'
