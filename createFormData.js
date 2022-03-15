ac8a10fb21db0c9b175433397a90e5cf71e5953d
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-03-15 09:23:20 -0300

    Update createFormData.js

--- createFormData.js
+++ createFormData.js
@@ -9,8 +9,21 @@
 // 	return formData
 // }
 
-function createFormData(formData, data = {}){
+// function createFormData(formData, data = {}){
 
+// 	Object.entries(data).map(([key, value]) => {
+
+// 		formData.append(key, value)
+// 	})
+// 	return formData
+// }
+
+function createFormData({
+	form: form,
+	data: data
+}){
+
+	const formData = new FormData(form) || new FormData
 	Object.entries(data).map(([key, value]) => {
 
 		formData.append(key, value)
