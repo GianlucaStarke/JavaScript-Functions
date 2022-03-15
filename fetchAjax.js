d6d39372ad560b7fab12a2ddcd0f0223d65c0455
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-03-15 15:43:10 -0300

    Update fetchAjax.js

--- fetchAjax.js
+++ fetchAjax.js
@@ -1,12 +1,12 @@
-/*
-* JavaScript fetch with JQuery Ajax similiar syntax
-* @param {string} path - file path or url to fetch
-* @param {string} method - PHP method: GET(default) or POST
-* @param {FormData} formData - FormData for fetch's body(optional)
-* @param {HTMLElement} form - form with datas for fetch's body(optional)
-* @param {object} data - datas for fetch's body(optional)
-* @param {string} dataType - response's data type: auto(default), json, blob or text
-*/
+/**
+ * JavaScript fetch with JQuery Ajax similiar syntax
+ * @param {string} path - file path or url to fetch
+ * @param {string} method - PHP method: GET(default) or POST
+ * @param {FormData} formData - FormData for fetch's body(optional)
+ * @param {HTMLElement} form - form with datas for fetch's body(optional)
+ * @param {object} data - datas for fetch's body(optional)
+ * @param {string} dataType - response's data type: auto(default), json, blob or text
+ */
 async function fetchAjax({
 	path: path,
 	method: method,
