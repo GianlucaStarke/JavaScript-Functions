ed5e1665edec7cb890c4e2d5d8b80d7bf7c94ef7
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-03-15 15:54:39 -0300

    Update fetchAjax.js

--- fetchAjax.js
+++ fetchAjax.js
@@ -1,9 +1,11 @@
+// createFormData() is required
+
 /**
  * JavaScript fetch with JQuery Ajax similiar syntax
  * @param {string} path - file path or url to fetch
  * @param {string} method - PHP method: GET(default) or POST
  * @param {FormData} formData - FormData for fetch's body(optional)
- * @param {HTMLElement} form - form with datas for fetch's body(optional)
+ * @param {HTMLElement} form - form element with datas for fetch's body(optional)
  * @param {object} data - datas for fetch's body(optional)
  * @param {string} dataType - response's data type: auto(default), json, blob or text
  */
