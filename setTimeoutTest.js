94e5f460ffd071096eaea49f30e77c30cbfaf238
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-05-31 15:41:47 -0300

    Update setTimeoutTest.js

--- setTimeoutTest.js
+++ setTimeoutTest.js
@@ -1,7 +1,10 @@
 const a = () => console.log('a');
-const b = async () => setTimeout(() => 'b', 1000);
-const c = async () => {
+const b = () => console.log('b');
+const c = () => {
 	setTimeout(a, 0);
-	console.log(await b());
+	
+	new Promise(resolve => resolve('b'))
+		.then(resolve => console.log(resolve));
+	
 	console.log('c');
 }
