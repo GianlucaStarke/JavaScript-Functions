ffb697e7edd98a6931bfd49e515771f416cdea53
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-06-01 16:37:04 -0300

    Update createHTMLElement.js

--- createHTMLElement.js
+++ createHTMLElement.js
@@ -1,32 +1,8 @@
-function createHTMLElement({
-    tag,
-    id,
-    inner_text,
-    classes
-}){
-    const idIsString = typeof id === 'string';
-    const innerTextIsString = typeof inner_text === 'string';
-    const classesIsArray = Array.isArray(classes);
-
+function createHTMLElement(tag, attr = {}){
     const element = document.createElement(tag);
-
-    if(idIsString){
-        element.id = id;
-    }
     
-    if(innerTextIsString){
-        element.innerText = inner_text;
-    }
+    Object.entries(attr).map(([attr, value]) => element.setAttribute(attr, value));
+    element.innerText = attr.innerText ? attr.innerText : '';
     
-    if(classesIsArray){
-        classes.map(
-            classe => (
-                element.classList.add(
-                    classe
-                )
-            )
-        );
-    }
-
     return element;
 }
