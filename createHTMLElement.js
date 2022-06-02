30abfa9b9d9232fa490a4d9aad0daeeecda69886
Gianluca Starke <48135682+XerlockHomeless@users.noreply.github.com>
2022-06-02 15:29:46 -0300

    Update createHTMLElement.js

--- createHTMLElement.js
+++ createHTMLElement.js
@@ -1,7 +1,13 @@
-function createHTMlElement(tag, props = {}){
+function createHTMLElement(tag, props = {}){
     const element = document.createElement(tag);
 
     Object.entries(props).map(([prop, value]) => element[prop] = value);
+	
+	Array.isArray(props.classes)
+		&& props.classes.map(classe => element.classList.add(classe));
+
+	Array.isArray(props.children)
+		&& props.children.map(child => element.append(child));
 
     return element;
 }
